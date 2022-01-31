9c5d375bc16c5791aef2fe8bc56201c7c6e3a288
Joaquin Kaplan <93784263+joaquinkaplan@users.noreply.github.com>
2022-01-31 07:32:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -22,28 +22,18 @@ function loadVoices(voices) {
 
 //DOM elements
 const selectElement = document.querySelector(".select");
-
 const moria = document.getElementById("moria");
+const mirtha = document.getElementById("mirtha");
+const carmen = document.getElementById("carmen");
+const amalia = document.getElementById("amalia");
+const marcela = document.getElementById("marcela");
+
 //App data
 const moria_frase = new SpeechSynthesisUtterance ("unos sandwichitos inmundos, donde el jamón no era jamón, era japaleta, todo berreta, todo berreta, y la dueña desesperada porque comiéramos, yo no como esto! cómo vas a comer harinas después de una función, qué sos una vaca, no puede ser");
-
-const mirtha = document.getElementById("mirtha");
-//App data
 const mirtha_frase = new SpeechSynthesisUtterance ("si te ven mal te maltratan, y si te ven bien te contratan");
-
-const carmen = document.getElementById("carmen");
-//App data
 const carmen_frase = new SpeechSynthesisUtterance ("somos tan hipócritas en este ambiente");
-
-const amalia = document.getElementById("amalia");
-//App data
 const amalia_frase = new SpeechSynthesisUtterance ("bien que te hubiese encantado que Robi williams te la ponga");
-
-const marcela = document.getElementById("marcela");
-//App data
 const marcela_frase = new SpeechSynthesisUtterance ("andate a cagar, te lo digo la verdad!");
-
-
 
 
 moria.addEventListener("click", (event) => {
