b979cd542b2026eb54e826d40d8afa3fdf22b525
Joaquin Kaplan <93784263+joaquinkaplan@users.noreply.github.com>
2022-03-15 05:04:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -25,14 +25,12 @@ const selectElement = document.querySelector(".select");
 const moria = document.getElementById("moria");
 const mirtha = document.getElementById("mirtha");
 const carmen = document.getElementById("carmen");
-const amalia = document.getElementById("amalia");
 const marcela = document.getElementById("marcela");
 
 //App data
 const moria_frase = new SpeechSynthesisUtterance ("unos sandwichitos inmundos, donde el jamón no era jamón, era japaleta, todo berreta, todo berreta, y la dueña desesperada porque comiéramos, yo no como esto! cómo vas a comer harinas después de una función, qué sos una vaca, no puede ser");
 const mirtha_frase = new SpeechSynthesisUtterance ("si te ven mal te maltratan, y si te ven bien te contratan");
 const carmen_frase = new SpeechSynthesisUtterance ("somos tan hipócritas en este ambiente");
-const amalia_frase = new SpeechSynthesisUtterance ("bien que te hubiese encantado que Robi williams te la ponga");
 const marcela_frase = new SpeechSynthesisUtterance ("andate a cagar, te lo digo la verdad!");
 
 
@@ -48,9 +46,6 @@ carmen.addEventListener("click", (event) => {
     speechSynthesis.speak(carmen_frase);
 })
 
-amalia.addEventListener("click", (event) => {
-    speechSynthesis.speak(amalia_frase);
-})
 
 marcela.addEventListener("click", (event) => {
     speechSynthesis.speak(marcela_frase);
