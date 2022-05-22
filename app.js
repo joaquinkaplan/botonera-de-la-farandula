a43f1f27048bfa82a9310bcf98946cb3f421978b
Joaquin Kaplan <93784263+joaquinkaplan@users.noreply.github.com>
2022-05-22 11:14:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ const selectElement = document.querySelector(".select");
 const moria = document.getElementById("moria");
 const mirtha = document.getElementById("mirtha");
 const carmen = document.getElementById("carmen");
-const marcela = document.getElementById("marcela");
+
 
 //App data
 const moria_frase = new SpeechSynthesisUtterance ("unos sandwichitos inmundos, donde el jamón no era jamón, era japaleta, todo berreta, todo berreta, y la dueña desesperada porque comiéramos, yo no como esto! cómo vas a comer harinas después de una función, qué sos una vaca, no puede ser");
 const mirtha_frase = new SpeechSynthesisUtterance ("si te ven mal te maltratan, y si te ven bien te contratan");
 const carmen_frase = new SpeechSynthesisUtterance ("somos tan hipócritas en este ambiente");
-const marcela_frase = new SpeechSynthesisUtterance ("andate a cagar, te lo digo la verdad!");
+
 
 
 moria.addEventListener("click", (event) => {
@@ -47,7 +47,4 @@ carmen.addEventListener("click", (event) => {
 })
 
 
-marcela.addEventListener("click", (event) => {
-    speechSynthesis.speak(marcela_frase);
-})
 
